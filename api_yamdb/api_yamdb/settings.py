f324fc28fac23e152db78279b82aba85980534d4
ShivaZoid <krovvar14@yandex.ru>
2023-04-05 13:40:31 +0800

    test_deploy_v2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,6 +12,7 @@ SECRET_KEY = (
     os.getenv('SECRET_KEY'),
     'my_secret_code_ilz@4zqj=rq&agdol^##zgl9(vs')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
